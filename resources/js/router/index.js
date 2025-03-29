c585f31e56b120fb14921102c20d78730534f78e
knivez13 <bonjourdeguzman@gmail.com>
2025-03-29 11:18:45 +0800

    update

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -12,32 +12,32 @@ const router = createRouter({
                 {
                     path: '',
                     meta: { access: 'user' },
-                    component: () => import('@/views/Dashboard.vue')
+                    component: () => import('@/views/landing/Landing.vue')
                 },
                 {
                     path: 'rent',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/landing/Rent.vue')
                 },
                 {
                     path: 'sell',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/landing/Sell.vue')
                 },
                 {
                     path: 'new-listing',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/landing/NewList.vue')
                 },
                 {
                     path: 'pricing',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/landing/Pricing.vue')
                 },
                 {
                     path: 'about-us',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/landing/AboutUs.vue')
                 }
             ]
         },
@@ -48,37 +48,37 @@ const router = createRouter({
                 {
                     path: 'dashboard',
                     meta: { access: 'user' },
-                    component: () => import('@/views/Dashboard.vue')
+                    component: () => import('@/views/setting/Dashboard.vue')
                 },
                 {
                     path: 'my-listing',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/setting/MyListing.vue')
                 },
                 {
                     path: 'create-property',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/setting/CreateProperty.vue')
                 },
                 {
                     path: 'message',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/setting/ChatRoom.vue')
                 },
                 {
                     path: 'subscription',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/setting/Subscription.vue')
                 },
                 {
                     path: 'favorites',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/setting/Favorites.vue')
                 },
                 {
                     path: 'save',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/setting/Save.vue')
                 }
             ]
         },
@@ -89,12 +89,12 @@ const router = createRouter({
                 {
                     path: 'dashboard',
                     meta: { access: 'user' },
-                    component: () => import('@/views/Dashboard.vue')
+                    component: () => import('@/views/admin/dashboard/Dashboard.vue')
                 },
                 {
                     path: 'message',
                     meta: { access: 'user' },
-                    component: () => import('@/views/uikit/FormLayout.vue')
+                    component: () => import('@/views/admin/message/ChatRoom.vue')
                 },
                 {
                     path: 'property-management',
@@ -102,22 +102,22 @@ const router = createRouter({
                         {
                             path: 'property-list',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/propertyManagement/PropertyList.vue')
                         },
                         {
                             path: 'for-approval',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/propertyManagement/ForApproval.vue')
                         },
                         {
                             path: 'active',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/propertyManagement/Active.vue')
                         },
                         {
                             path: 'reject',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/propertyManagement/Reject.vue')
                         }
                     ]
                 },
@@ -127,37 +127,37 @@ const router = createRouter({
                         {
                             path: 'amenity',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/Amenity.vue')
                         },
                         {
                             path: 'appliances',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/Appliances.vue')
                         },
                         {
                             path: 'file-type',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/FileType.vue')
                         },
                         {
                             path: 'mode-of-payment',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/ModeOfPayment.vue')
                         },
                         {
                             path: 'price-type',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/PriceType.vue')
                         },
                         {
                             path: 'property-type',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/PropertyType.vue')
                         },
                         {
                             path: 'property-status',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/admin/maintenance/PropertyStatus.vue')
                         }
                     ]
                 },
@@ -167,12 +167,12 @@ const router = createRouter({
                         {
                             path: 'user-list',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/users/UserList.vue')
                         },
                         {
                             path: 'role',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/users/Role.vue')
                         }
                     ]
                 },
@@ -182,22 +182,22 @@ const router = createRouter({
                         {
                             path: 'user-subscription',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/subscription/UserSubscription.vue')
                         },
                         {
                             path: 'expired-month',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/subscription/ExpiredMonth.vue')
                         },
                         {
                             path: 'for-approval',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/subscription/ForApproval.vue')
                         },
                         {
                             path: 'subscription-type',
                             meta: { access: 'user' },
-                            component: () => import('@/views/Dashboard.vue')
+                            component: () => import('@/views/subscription/SubscriptionType.vue')
                         }
                     ]
                 }
---
+++ resources/js/views/setting/MyListing.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+    <div class="grid grid-cols-12 gap-8"></div>
+</template>
