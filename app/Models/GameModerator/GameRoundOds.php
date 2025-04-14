a5e701bd399bca546c68c2aa466adc42b4ba7a42
knivez13 <bonjourdeguzman@gmail.com>
2025-04-14 12:45:26 +0800

    update

--- app/Models/GameModerator/GameRoundOds.php
+++ app/Models/GameModerator/GameRoundOds.php
@@ -2,10 +2,28 @@
 
 namespace App\Models\GameModerator;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Traits\Uuids;
+use App\Traits\LocalTimestamps;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GameRoundOds extends Model
 {
-    use HasFactory;
+    use HasFactory, Uuids, SoftDeletes, LocalTimestamps;
+    protected $guarded = [];
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by')->select('id', 'name');
+    }
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by')->select('id', 'name');
+    }
+    public function deletedBy()
+    {
+        return $this->belongsTo(User::class, 'deleted_by')->select('id', 'name');
+    }
 }
